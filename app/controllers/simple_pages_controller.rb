937a31b8b8fe5667376addce983aaa2468bb874a
Josh Bromehead <josh.bromehead@mail.com>
2018-03-06 12:44:46 -0600

    Displaying dynamic data, using partials

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -2,9 +2,10 @@ class SimplePagesController < ApplicationController
   def index
   end
 
+
+
   def landing_page
-    @featured_product = Product.first
+    @products = Product.limit(3)
   end
 
-  
 end
