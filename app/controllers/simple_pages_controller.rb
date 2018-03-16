ac050bb1bd9e1b7a07e0507da34eaefc2ce5ff64
Josh Bromehead <josh.bromehead@mail.com>
2018-03-16 15:19:26 -0500

    5.2 testing live email facility

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -8,4 +8,14 @@ class SimplePagesController < ApplicationController
     @products = Product.all
   end
 
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+        to: 'jmbromehead@gmail.com',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :products
+  post 'simple_pages/thank_you'
+
   get 'simple_pages/about'
 
   get 'simple_pages/contact'
@@ -7,6 +9,9 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
 
   root 'simple_pages#landing_page'
+
+  post 'simple_pages/thank_you'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :orders, only: [:index, :show, :create, :destroy]
 
