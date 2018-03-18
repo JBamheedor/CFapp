7f59b7009a7a130ee2d283f97dbfd5d6f12c956c
Josh Bromehead <josh.bromehead@mail.com>
2018-03-18 12:24:03 -0500

    5.3 - checking email working

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,12 @@
 class Product < ApplicationRecord
   has_many :orders
 
+# Checking which environment production or development because Postgres matching operator LIKE is case sensitive
   def self.search(search_term)
+    if Rails.env.production?
+    Product.where("name ilike?", "%#{search_term}%")
+    else
     Product.where("name LIKE ?", "%#{search_term}%")
+    end
   end
 end
