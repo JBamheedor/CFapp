8d106b127db807ac4ac689b050a0ebd9d1188c8c
Josh Bromehead <josh.bromehead@mail.com>
2018-03-21 19:16:23 -0500

    5.7 Adding comment model and styling

---
+++ app/assets/javascripts/effects.coffee
@@ -0,0 +1,4 @@
+
+$(document).on 'turbolinks:load', ->
+  $('.alert').delay(1000).fadeOut 2000
+  return
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
 
 # Checking which environment production or development because Postgres matching operator LIKE is case sensitive
   def self.search(search_term)
@@ -9,4 +10,13 @@ class Product < ApplicationRecord
     Product.where("name LIKE ?", "%#{search_term}%")
     end
   end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
+
 end
