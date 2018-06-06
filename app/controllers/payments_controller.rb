af92640b6029e1d70b2eac4b19a0957aa0ad137e
JBamheedor <josh.bromehead@mail.com>
2018-06-06 13:40:18 -0500

    Ghost btn update & cents payment to stripe

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,36 +3,33 @@ class PaymentsController < ApplicationController
 
   def create
     @product = Product.find(params[:product_id])
-
-    if user_signed_in?
-      @user = current_user
-    else
-      redirect_to new_user_session
-    end
+    @user = current_user
 
     token = params[:stripeToken]
     # Create the charge on Stripe's servers - this will charge the user's card
 
     begin
       charge = Stripe::Charge.create(
-        amount: (@product.price).to_i,
-        currency: 'usd',
-        source: token,
-        description: params[:stripeEmail],
+        amount:        @product.price * 100, 
+        currency:      'usd',
+        source:        token,
+        description:   params[:stripeEmail],
         receipt_email: params[:stripeEmail]
       )
 
-    if charge.paid
-      Order.create(product_id: @product.id, user_id: @user.id, total: @product.price)
+      if charge.paid
+        Order.create(product_id: @product.id, user_id: @user.id, total: @product.price)
+        flash[:notice] = "Your payment was processed successfully"
+      end
+
+    # this will never really reached when using the checkout.js
+    rescue Stripe::CardError => e
+      #Declined card
+      body          = e.json_body
+      err           = body[:error]
+      flash[:alert] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
     end
 
-  rescue Stripe::CardError => e
-    #Declined card
-    body = e.json_body
-    err = body[:error]
-    flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
-  end
-  
     redirect_to product_path(@product)
 
   end
